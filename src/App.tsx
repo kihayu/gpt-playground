2ca9dd88028f78bd4f0624bfd626c62c2a2f21c6
Hie Keanu <hie.keanu@gmail.com>
2024-02-07 20:55:42 +0100

    feat: add sample messages and some styling improvements

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Radio } from 'react-aria-components'
 import { TextArea } from './components/TextArea'
 import { Button } from './components/Button'
 import './App.css'
+import { ChatMessage } from './ChatMessage'
 
 function App() {
   const [userInput, setUserInput] = React.useState('')
@@ -27,14 +28,34 @@ function App() {
   return (
     <div className="app">
       <div className="app-system">
-        <TextArea className="react-aria-TextField app-system-input-wrapper" inputClassName='app-system-input' label="System" />
+        <TextArea
+          className="react-aria-TextField app-system-input-wrapper"
+          inputClassName="app-system-input"
+          label="System"
+        />
       </div>
       <div className="app-chat">
-        <div className="app-messages"></div>
+        <div className="app-messages">
+          <ChatMessage author="System" message="How are you?" />
+          <ChatMessage author="User" message="I'm fine, thank you!" />
+          <ChatMessage author="System" message="What are you up to?" />
+          <ChatMessage author="User" message="Just working on some code." />
+          <ChatMessage author="System" message="Do you need any help?" />
+          <ChatMessage author="User" message="Yes, I'm having trouble with a function." />
+          <ChatMessage author="System" message="Can you describe the problem?" />
+          <ChatMessage author="User" message="I'm getting an undefined error." />
+          <ChatMessage author="System" message="Have you checked the variable declarations?" />
+          <ChatMessage author="User" message="Let me check." />
+          <ChatMessage author="System" message="Take your time." />
+          <ChatMessage author="User" message="I found the issue, thanks!" />
+          <ChatMessage author="System" message="You're welcome! Anything else you need?" />
+          <ChatMessage author="User" message="No, that's all for now." />
+          <ChatMessage author="System" message="Alright, happy coding!" />
+        </div>
         <div className="app-user-message">
           <TextField
             className="react-aria-TextField app-user-message-input-wrapper"
-            inputClassName='app-user-message-input'
+            inputClassName="app-user-message-input"
             label="Eingabe"
             value={userInput}
             onChange={setUserInput}
