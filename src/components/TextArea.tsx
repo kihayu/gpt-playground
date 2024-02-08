e345eb3770f2892cbc0d0946c9dd0d00426fed25
Keanu Hie <keanu.hie@strg.at>
2024-02-08 15:26:53 +0100

    feat: add placeholder to TextArea

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -13,15 +13,16 @@ import './TextArea.css'
 export interface TextFieldProps extends AriaTextFieldProps {
   label?: string
   description?: string
+  placeholder?: string
   errorMessage?: string | ((validation: ValidationResult) => string)
   inputClassName?: string
 }
 
-export function TextArea({ label, description, errorMessage, inputClassName, ...props }: TextFieldProps) {
+export function TextArea({ label, description, placeholder, errorMessage, inputClassName, ...props }: TextFieldProps) {
   return (
     <AriaTextField {...props}>
       <Label>{label}</Label>
-      <AriaTextArea className={inputClassName} />
+      <AriaTextArea className={inputClassName} placeholder={placeholder} />
       {description && <Text slot="description">{description}</Text>}
       <FieldError>{errorMessage}</FieldError>
     </AriaTextField>
