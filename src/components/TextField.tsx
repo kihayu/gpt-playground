e99d561efa44715f13d1fad9c8b2d78197eb086e
Keanu Hie <keanu.hie@strg.at>
2024-02-08 16:02:31 +0100

    feat: add placeholder to standard TextField

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -13,15 +13,16 @@ import './TextField.css'
 export interface TextFieldProps extends AriaTextFieldProps {
   label?: string
   description?: string
+  placeholder?: string
   errorMessage?: string | ((validation: ValidationResult) => string)
   inputClassName?: string
 }
 
-export function TextField({ label, description, errorMessage, inputClassName, ...props }: TextFieldProps) {
+export function TextField({ label, description, placeholder, errorMessage, inputClassName, ...props }: TextFieldProps) {
   return (
     <AriaTextField {...props}>
       <Label>{label}</Label>
-      <Input className={inputClassName} />
+      <Input className={inputClassName} placeholder={placeholder} />
       {description && <Text slot="description">{description}</Text>}
       <FieldError>{errorMessage}</FieldError>
     </AriaTextField>
